16d49132daf39a8ee5116df27ed90a8ac80673a8
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-10-16 13:12:00 +0300

    add Picture-in-Picture icon and update button to use it

--- react/features/picture-in-picture-web/components/WebPictureInPictureButton.ts
+++ react/features/picture-in-picture-web/components/WebPictureInPictureButton.ts
@@ -1,20 +1,23 @@
-import { WithTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 
 import { IReduxState } from '../../app/types';
-import { IconArrowDown } from '../../base/icons/svg';
+import { translate } from '../../base/i18n/functions';
+import { IconPictureInPicture } from '../../base/icons/svg';
 import AbstractButton, { IProps as AbstractButtonProps } from '../../base/toolbox/components/AbstractButton';
 import { toggleWebPip } from '../actions';
 
-interface IProps extends AbstractButtonProps, WithTranslation {
+interface IProps extends AbstractButtonProps {
     _inPip: boolean;
 }
 
+/**
+ * Implements an {@link AbstractButton} to open the applications page in a new window.
+ */
 class WebPictureInPictureButton extends AbstractButton<IProps> {
-    override accessibilityLabel = 'toolbar.pip';
-    override icon = IconArrowDown;
-    override label = 'toolbar.pip';
-    override tooltip = 'toolbar.pip';
+    override accessibilityLabel = 'toolbar.accessibilityLabel.pip';
+    override icon = IconPictureInPicture;
+    override label = 'toolbar.accessibilityLabel.pip';
+    override tooltip = 'toolbar.accessibilityLabel.pip';
 
     override _handleClick() {
         const { dispatch } = this.props;
@@ -27,10 +30,18 @@ class WebPictureInPictureButton extends AbstractButton<IProps> {
     }
 }
 
+
+/**
+ * Maps part of the redux state to the component's props.
+ *
+ * @param {Object} state - The redux store/state.
+ * @returns {Object}
+ */
 function _mapStateToProps(state: IReduxState) {
     const { inPip } = state['features/picture-in-picture-web'] || { inPip: false };
 
     return { _inPip: inPip };
 }
 
-export default connect(_mapStateToProps)(WebPictureInPictureButton);
+
+export default translate(connect(_mapStateToProps)(WebPictureInPictureButton));
